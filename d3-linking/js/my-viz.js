e95554b3cda09b06e6186b11227d8127cb37a6d7
Alejandro J. Rojas <alejandrorojas@Alejandros-Air.fios-router.home>
2017-04-19 01:42:22 -0400

    d3 code fixed

--- d3-linking/js/my-viz.js
+++ d3-linking/js/my-viz.js
@@ -10,7 +10,12 @@ d3.csv("data/movie_list_d3.csv",function(data) {
     
     console.log(data);
 
-    var rev = data.map(function(d) { return +d.revenues_clean/1000000; }).filter(function(d) { return d < 1000; });
+    var filtered_data = data.filter(function(d) { return (+d.revenues_clean/1000000) < 1500; })
+                   .filter(function(d) { return d3.isoParse(d.release_date) > d3.isoParse('01-01-2000'); })
+              
+
+    var rev =  filtered_data.map(function(d) { return +d.revenues_clean/1000000; })
+
     
     var barx = d3.scaleLinear()
         //.domain(d3.extent(rev))
@@ -40,7 +45,7 @@ d3.csv("data/movie_list_d3.csv",function(data) {
         .attr("x", 1)
         .attr("width", barx(bins[0].x1) - barx(bins[0].x0) - 1)
         .attr("height", function(d) { return 280 - bary(d.length); })
-        .attr("fill","red")
+        .attr("fill", function(d) { return "rgb(0, 0, " + (d.length * 10) + ")"})
         .on("click",function(d,i) {
             // console.log(d);
             // console.log(i);
@@ -71,12 +76,12 @@ d3.csv("data/movie_list_d3.csv",function(data) {
 
  
     //var format = d3.timeFormat("%m-%d-%Y")
-    var times = data.filter(function(d) { return d3.isoParse(d.release_date) > d3.isoParse('01-01-2000') &&  d3.isoParse(d.release_date) < d3.isoParse('12-31-2017');}).map(function(d) { return d3.isoParse(d.release_date); });
+    var times = filtered_data.map(function(d) { return d3.isoParse(d.release_date); });
    console.log(times);
 
     var timex = d3.scaleLinear()
         //.domain(d3.extent(times))
-        .domain([d3.isoParse('01-01-2000'), d3.isoParse('12-31-2016') ])
+        .domain([d3.isoParse('01-01-1999'), d3.isoParse('12-31-2017') ])
         .range([40, 600]);
 
     var timey = d3.scaleLinear()
@@ -84,37 +89,51 @@ d3.csv("data/movie_list_d3.csv",function(data) {
         .range([280,10]);
 
 
-
+    var profit_color = filtered_data.map(function(d){if (+d.profit_class > 0){return "green"} else {return "red"} });
+    var formatPercent = d3.format(",.0%")
+    var return_label = filtered_data.map(function(d){ return "ROI = "+ formatPercent(d.return)});
+    console.log(profit_color[0].toString())
     timeseriessvg.selectAll(".circle")
-        .data(data)
+        .data(filtered_data)
         .enter()
         .append("circle")
         .attr("cx",function(d,i) { return timex(times[i]); })
         .attr("cy",function(d,i) { return timey(rev[i]); })
         .attr("r",4)
-        .attr("fill","red")
-        .style("opacity",0.3);
+        .attr("fill", function(d,i) { return profit_color[i] })
+        .style("opacity",0.3)
+        .on("mouseover", function(d,i) {
+//Create the tooltip label
+        timeseriessvg.append("text")
+            .attr("id", "tooltip1")
+            .text(d.title_clean)
+            .attr("dx",timex(times[i]))
+            .attr("dy",timey(rev[i]+200))
+            .attr("text-anchor", "end")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "13px")
+            .attr("font-weight", "bold")
+            .attr("fill", "gray");
+        timeseriessvg.append("text")
+            .attr("id", "tooltip2")            
+            .text(return_label[i])
+            .attr("dx",timex(times[i]))
+            .attr("dy",timey(rev[i]+120))
+            .attr("text-anchor", "end")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "11px")
+            .attr("font-style", "italic")
+            .attr("fill", profit_color[i])
 
-    var tooltip = d3.select("body")
-    
-    timeseriessvg.selectAll(".circle")
-        .data(data)
-        .enter()
-        .append("text")
-        .attr("dx",function(d,i) { return timex(times[i]); })
-        .attr("dy",function(d,i) { return timey(rev[i]); })
-        .text(function(d) { return d.title_clean })
-        .style("visibility","hidden")
-        .on("mouseover", function(d)
-        {
-            d3.select(this).style("visibility","visible")
-                })
-        .on("mouseout", function(d)
-        {
-            d3.select(this).style("visibility","hidden")
+
+            ;
         })
-        .style("visibility","visible")
+        .on("mouseout", function() {
+//Remove the tooltip
+            d3.select("#tooltip1").remove();
+            d3.select("#tooltip2").remove();
 
+        })
 
 
     timeseriessvg.append("g")
@@ -125,5 +144,4 @@ d3.csv("data/movie_list_d3.csv",function(data) {
     timeseriessvg.append("g")
         .attr("class", "axis axis--x")
         .attr("transform", "translate(40," + 00 + ")")
-        .call(d3.axisLeft(timey));
-});
+        .call(d3.axisLeft(timey));})
